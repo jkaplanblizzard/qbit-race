c7701d6670ff27479591b6ceefd43572a66ca3d5
jkaplanblizzard <132863412+jkaplanblizzard@users.noreply.github.com>
2023-05-17 20:51:26 -0600

    Update auth.js

--- build/src/qbittorrent/auth.js
+++ build/src/qbittorrent/auth.js
@@ -3,27 +3,20 @@ import { login as apiLogin, QbittorrentApi } from './api.js';
 
 export const loginV2 = async (qbittorrentSettings) => {
   const logger = getLoggerV3();
-  const { url, method, headers, data } = qbittorrentSettings;
-
-  console.log('Request URL:', url);
-  console.log('Request Method:', method);
-  console.log('Request Headers:', headers);
-  console.log('Request Body:', data);
-
   const response = await apiLogin(qbittorrentSettings);
   // TODO: Differentiate between wrong credentials vs. qbit is not listening (wrong URL / port etc.)
 
-  console.log('Response:', response.data);
-  console.log('Response Code:', response.status);
-
   let cookiesArray = [];
-  if (response.headers['set-cookie']) {
-    cookiesArray = response.headers['set-cookie'].split(';');
+
+  if (Array.isArray(response.headers['set-cookie'])) {
+    cookiesArray = response.headers['set-cookie'];
+  } else if (typeof response.headers['set-cookie'] === 'string') {
+    cookiesArray = [response.headers['set-cookie']];
   }
 
   if (cookiesArray.length === 0) {
     throw new Error(`Failed to authenticate`);
   }
 
-  return new QbittorrentApi(url, cookiesArray[0]);
+  return new QbittorrentApi(qbittorrentSettings.url, cookiesArray[0]);
 };

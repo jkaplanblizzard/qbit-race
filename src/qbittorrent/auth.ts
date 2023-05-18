ccca59325dc5ec924e9c394fadc351edf17daa28
jkaplanblizzard <132863412+jkaplanblizzard@users.noreply.github.com>
2023-05-17 22:25:29 -0600

    Update auth.ts

--- src/qbittorrent/auth.ts
+++ src/qbittorrent/auth.ts
@@ -3,16 +3,35 @@ import { QBITTORRENT_SETTINGS, Settings } from '../utils/config.js';
 import { getLoggerV3 } from '../utils/logger.js';
 import { login as apiLogin, QbittorrentApi } from './api.js';
 
-
 export const loginV2 = async (qbittorrentSettings: QBITTORRENT_SETTINGS): Promise<QbittorrentApi> => {
     const logger = getLoggerV3();
+    const { username, password } = qbittorrentSettings;
 
-    const response = await apiLogin(qbittorrentSettings);
+    console.log('Username:', username);
+    console.log('Password:', password);
 
-    // TODO: Differentiate between wrong credentials vs. qbit is not listening (wrong URL / port etc.)
-    if (Array.isArray(response.headers['set-cookie']) === false || response.headers['set-cookie'].length === 0) {
-        throw new Error(`Failed to authenticate`);
+    try {
+        const response = await apiLogin(qbittorrentSettings);
+        // TODO: Differentiate between wrong credentials vs. qbit is not listening (wrong URL / port etc.)
+
+        console.log('Response Headers:', response.headers);
+        console.log('Response Body:', response.data);
+
+        let cookiesArray: string[] = [];
+
+        if (Array.isArray(response.headers['set-cookie'])) {
+            cookiesArray = response.headers['set-cookie'];
+        } else if (typeof response.headers['set-cookie'] === 'string') {
+            cookiesArray = [response.headers['set-cookie']];
+        }
+
+        if (cookiesArray.length === 0) {
+            throw new Error(`Failed to authenticate`);
+        }
+
+        return new QbittorrentApi(qbittorrentSettings.url, cookiesArray[0]);
+    } catch (error) {
+        console.log('Error:', error);
+        throw error;
     }
-
-    return new QbittorrentApi(qbittorrentSettings.url, response.headers['set-cookie'][0]);
-}
+};
